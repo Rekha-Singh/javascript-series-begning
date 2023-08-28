de67f2877c4f87e4feb0303684e0fd1f540c7fdd
Rekha Singh <101234701+Rekha-Singh@users.noreply.github.com>
2023-08-28 17:48:40 +0000

    vaiables done

---
+++ basics_of_js/variables.js
@@ -0,0 +1,8 @@
+const accId=7546456
+let gmail='1503199rekha@gmail.com'
+// console.log(gmail)
+var pass='rekha200'
+// console.log(pass)
+// accId=90
+// console.log(accId)
+console.table([accId,gmail,pass])

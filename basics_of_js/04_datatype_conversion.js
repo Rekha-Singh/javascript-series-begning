5bc206e0b7a0e08a25a75cc84611efaddd3eb43e
Rekha Singh <101234701+Rekha-Singh@users.noreply.github.com>
2023-08-29 09:03:15 +0000

    conversion done

--- basics_of_js/04_datatype_conversion.js
+++ basics_of_js/04_datatype_conversion.js
@@ -1,6 +1,20 @@
-let score=45
-console.log(typeof(score))
-let mynumber="4547675"
-console.log(typeof mynumber)
-let numbet=Number(mynumber)
-console.log(typeof numbet)
+// let score=45
+// console.log(typeof(score))
+
+// case for number
+
+// let mynumber="4547675abscs"
+// console.log(typeof mynumber)
+// let numbet=Number(mynumber)
+// console.log(typeof numbet)
+// console.log(numbet)
+
+// case for null
+
+let mynumber=null
+console.log(typeof(mynumber))// object
+let num=Number(mynumber)
+console.log(typeof num)// number
+console.log(num)// 0
+
+

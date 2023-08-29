4356152b832ce92a6f356fc18ae46b577afb1bf8
Rekha Singh <101234701+Rekha-Singh@users.noreply.github.com>
2023-08-29 08:32:17 +0000

    datatypes done

---
+++ basics_of_js/datatypes.js
@@ -0,0 +1,21 @@
+// alert(3+8)   // showing error because we are using node js not browser
+console.log("rekha");console.log(6)// code readablity is not good so it should be good(high)
+
+let name="rekha"
+let age=23
+let isName=false
+console.log(typeof(isName))
+/*
+1.number=2  to power 53
+2.bigint
+3.boolean=true/false
+4.string=""/''
+5.null=standalone value
+6.undefind=not defined value
+7.sysmbol=uniques 
+8.objects=key and values
+ */
+
+
+// type of null =object in js
+// type of undefined=undefined

1d4ef6ac5a59fc03f8ef3da5cde66ba71e1834f5
Rekha Singh <101234701+Rekha-Singh@users.noreply.github.com>
2023-08-29 09:12:28 +0000

    conversion done

--- basics_of_js/04_datatype_conversion.js
+++ basics_of_js/04_datatype_conversion.js
@@ -3,7 +3,7 @@
 
 // case for number
 
-// let mynumber="4547675abscs"
+// let mynumber="4547675abscs"  // NaN=not a number
 // console.log(typeof mynumber)
 // let numbet=Number(mynumber)
 // console.log(typeof numbet)
@@ -12,9 +12,23 @@
 // case for null
 
 let mynumber=null
-console.log(typeof(mynumber))// object
-let num=Number(mynumber)
-console.log(typeof num)// number
-console.log(num)// 0
+// console.log(typeof(mynumber))// object
+// let num=Number(mynumber)
+// console.log(typeof num)// number
+// console.log(num)// 0
+
+// 1=true, 0=false
+
+// for numbers
+
+// let isName=1
+// let isMyName=Boolean(isName)
+// console.log(isMyName)// true
+
+// for strings
+let isName='r'// true 
+let isMyName=Boolean(isName)
+console.log(isMyName)
+
 
 

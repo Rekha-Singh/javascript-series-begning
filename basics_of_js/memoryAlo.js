ab675b7fb4e58617b3b8870990578ac25a56d70c
Rekha Singh <101234701+Rekha-Singh@users.noreply.github.com>
2023-09-04 06:50:27 +0000

    memory allocation in js done

--- basics_of_js/memoryAlo.js
+++ basics_of_js/memoryAlo.js
@@ -2,7 +2,19 @@
 // stack =its provides memory for all primitive data types and create the copies of objects
 // heap= non primitive datatypes and create reference for the objects
 
-
+// stack
 let myName="rekha"
 let altName=myName
+altName="mayaSingh"
 console.log(altName);
+
+// for heap
+ const user={
+    gmail:"6456345rekha@gmail.com",
+    pin:462001
+ }
+ const anotherUser=user
+
+ console.log(anotherUser);
+ console.log(user.pin);
+ console.log(anotherUser.pin);

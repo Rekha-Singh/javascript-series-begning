bc6f9dc23c3794cf0fb3b7f2e44e73891da02e39
Rekha Singh <101234701+Rekha-Singh@users.noreply.github.com>
2023-09-04 06:28:28 +0000

    memory allocation in js

---
+++ basics_of_js/memoryAlo.js
@@ -0,0 +1,8 @@
+// there are two types of memory =stack and heap
+// stack =its provides memory for all primitive data types and create the copies of objects
+// heap= non primitive datatypes and create reference for the objects
+
+
+let myName="rekha"
+let altName=myName
+console.log(altName);

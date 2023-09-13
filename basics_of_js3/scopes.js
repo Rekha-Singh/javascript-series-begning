3a8e07dff8c54c54af2317b152d6a55f279d834d
Rekha Singh <101234701+Rekha-Singh@users.noreply.github.com>
2023-09-13 12:06:00 +0000

    scopes in js

---
+++ basics_of_js3/scopes.js
@@ -0,0 +1,23 @@
+
+// console.log(a);
+
+// function checkScope(){
+//     console.log(a);
+//     a=200
+//     console.log(a);
+//     console.log(b);
+//     // b=600// constant val not assign aftr assignning
+//     console.log(c);
+// }
+// checkScope()
+
+if(true){ // block scope
+    // a='rekha'
+    let a=20;
+const b=30
+var c=50
+ 
+}
+// console.log(a);
+// console.log(b);
+console.log(c);

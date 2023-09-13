e3bb95558f13ec8e195ffe632d90ba1edd8e3eb9
Rekha Singh <101234701+Rekha-Singh@users.noreply.github.com>
2023-09-13 12:41:08 +0000

    nested scope

---
+++ basics_of_js3/nestedScope.js
@@ -0,0 +1,31 @@
+function one(){
+    name='Rameesha'
+    function two(){
+        const website='https://github.com/Rekha-Singh/javascript-series-begning'
+        // console.log(name);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+if(true){
+    const username='rekha'
+    if(username==='rekha'){
+        const web=' google.com'
+        // console.log(username+web);
+    }
+    // console.log(web);
+}
+// console.log(username);
+console.log(one(8));
+function one(num) {
+    return num+1
+}
+// console.log(one(7));
+
+console.log(twoAdd(8));
+const twoAdd=function(num){
+    return num+3
+}
+// console.log(twoAdd(8));

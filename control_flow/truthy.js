c6a39e36a4c8f49b6124a6b3d74780cadabb600d
Rekha Singh <101234701+Rekha-Singh@users.noreply.github.com>
2023-09-16 17:41:16 +0000

    truth or false=truthy

---
+++ control_flow/truthy.js
@@ -0,0 +1,38 @@
+const email='agfhdfg@.com'
+// if (email) {
+//     console.log("User have email");
+// }
+// else{
+//     console.log("dont have email");
+// }
+
+// false,0,-0,bigInt 0n,null,NaN,"",'',undefined=all are false values 
+
+// truthy value='0','false',[],{},function (){}
+
+// array check
+
+//  const getemail=[]
+//  if (getemail.length===0) {
+//     console.log('Array is empty');
+//  }else{
+//     console.log('email not found');
+//  }
+
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0){
+//     console.log("your obj is empty");
+// }
+// else{
+//     console.log("Not a empty array");
+// }
+
+// nullish coelisin operator(??)
+
+let val1
+// val1=5??10
+// val1=null?? 30
+// val1=undefined??50??null
+val1=5??10??15
+console.log(val1);
+

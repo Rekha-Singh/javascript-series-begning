8aef92fd53fbf2f01fd21615e19024ffdd09baf3
Rekha Singh <101234701+Rekha-Singh@users.noreply.github.com>
2023-09-16 16:46:17 +0000

    control flow

---
+++ control_flow/if_else.js
@@ -0,0 +1,41 @@
+
+// <,>,<=,>=,==,===,!=,!==;
+
+
+// const isValid=true;
+// if (isValid) {
+//     console.log("Executed if block");
+// }
+// else{
+//     console.log("else block executed");
+// }
+
+const pow=2
+// if(true){
+//     console.log(`power is ${pow}`);
+// }
+// else{
+//     console.log("else block executed");
+// }
+
+
+const balance=1200
+// if(balance<500){
+//     console.log("balance less then 500");
+// }else if (balance<700) {
+//     console.log("balance less then 700");
+// } else if(balance<850){
+//     console.log("balance less then 850");
+// }
+// else{
+//     console.log(" balance is greater then 1000");
+// }
+
+const isUId=545
+const isCreditcard=true;
+if (isUId && isCreditcard && 2==3) {
+    console.log("you are eligible to shopping");
+}
+else{
+    console.log('not eligible for shopping');
+}

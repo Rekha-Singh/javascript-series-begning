cdade25c315221c96fd3f2fce47e3d252e04b8d8
Rekha Singh <101234701+Rekha-Singh@users.noreply.github.com>
2023-09-17 16:06:06 +0000

    do while loop

---
+++ itrator/do_while_loop.js
@@ -0,0 +1,17 @@
+let score=0
+do {
+//    console.log(`your is score is ${score}`); 
+   score++;
+}
+ while (score<=10)
+// {
+//     console.log(`your is score is ${score}`);
+    
+// }
+
+
+let score1=80
+do{
+    console.log(`value for score is ${score1}`);
+    score1++
+}while(score1<=60)

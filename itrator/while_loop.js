95a091ab8c0919d9fa13ff7d20e2b56265d28586
Rekha Singh <101234701+Rekha-Singh@users.noreply.github.com>
2023-09-17 15:54:43 +0000

    while loop completed

---
+++ itrator/while_loop.js
@@ -0,0 +1,12 @@
+let i=0
+while (i<=10) {
+    // console.log(` i is = ${i}`);
+    i=i+2
+}
+
+let myArr=['radhaji','Krishn ji','shivji','Parvati ji']
+let arr=0
+while (arr<myArr.length) {
+    console.log(`value of ${arr} is = ${myArr[arr]} `);
+    arr++
+}
